bb0a8c4105841a6ac50bb66dd28538fd57249ed2
Aditya <webmasteradi@gmail.com>
2015-10-05 21:28:49 +0530

    Initial version

---
+++ background.js
@@ -0,0 +1,78 @@
+
+//for connecting to codeforces.com
+var recent_url = "http://codeforces.com/api/user.status?handle=";
+var valid_url = " http://codeforces.com/api/user.info?handles=";
+var request_url="";
+var start = 1;
+var count  = 1000;
+
+
+var link = "http://codeforces.com/contest/";
+// var list = [];
+
+// function getSubmission() {
+var name='';
+// function getname() {
+chrome.storage.sync.get("username", function(items) {
+			if (!chrome.runtime.error) {
+				console.log(items);
+			// 	document.getElementById("data").innerText = items.data;
+			name = items.username;
+			console.log('name '+name);
+			}
+});
+ chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (key in changes) {
+          var storageChange = changes[key];
+          console.log('Storage key "%s" in namespace "%s" changed. ' +
+                      'Old value was "%s", new value is "%s".',
+                      key,
+                      namespace,
+                      storageChange.oldValue,
+                      storageChange.newValue);
+        }
+        name = changes['username'].newValue;
+      });
+// }
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+  if (request.action == "xhttp") {
+
+  		// getname();
+        var xhttp = new XMLHttpRequest();
+        var method = request.method ? request.method.toUpperCase() : 'GET';
+
+        xhttp.onload = function() {
+            // list  = parseData(JSON.parse(xhttp.responseText));
+            // callback(list);
+
+
+            callback(xhttp.responseText);
+        };
+        xhttp.onerror = function() {
+            // Do whatever you want on error. Don't forget to invoke the
+            // callback to clean up the communication port.
+            console.log("error");
+            callback();
+        };
+        console.log("background user  "+request.usern);
+        console.log("usename  "+name);
+        if (method == "POST") {
+        	console.log("check user  "+request.usern);
+            // xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            request_url = valid_url+request.usern;
+            xhttp.open('GET', request_url, true);
+        }
+        else {
+        
+        request_url = recent_url+name+"&from="+start+"&count="+count;
+        // var request_url = recent_url+request.usern+"&from="+start+"&count="+count;
+        xhttp.open('GET', request_url, true);
+        }
+        xhttp.send();
+        return true; // prevents the callback from being called too early on return
+    }
+});
+
+
+ // window.setInterval(chrome.storage.sync.get, 10);
+
---
+++ options.js
@@ -0,0 +1,83 @@
+// Saves options to chrome.storage
+var defaultuser = "";
+
+function save_options() {
+
+  var val = user.value;
+  val = val.trim();
+  console.log('val  '+val);
+  var status = document.getElementById('status');
+   status.textContent = '';
+  var flg =0;
+  chrome.runtime.sendMessage({
+  
+  method: 'POST',
+  action: 'xhttp',
+  
+  usern: val,
+  // username: 'webmasteradi',
+	}, function(responseText) {
+	    // console.log(responseText);  
+	    console.log("true");
+	    flg = checkUser((responseText));
+	    setUser(flg,val,status);
+	    
+});
+  
+  console.log("flg 2 "+flg);
+ 
+}
+
+
+function restore_options() {
+ 
+  
+}
+
+function checkUser(text) {
+  
+    var obj = JSON.parse(text);
+    // console.log(obj);
+    console.log("valid : "+obj.status);
+ 	if(obj.status == "OK") {
+ 		return true;
+ 	}
+ 	else {
+ 		return false;
+ 	}
+    
+}
+function setUser(flg,val,status){
+	 if(flg){
+  chrome.storage.sync.set({
+    
+    "username": val
+    
+  }, function() {
+    // Update status to let user know options were saved.
+    
+    if (chrome.runtime.error) {
+			console.log("Runtime error.");
+			status.textContent = 'Sorry options cannot be saved now.';
+		}
+		else {
+    status.textContent = 'Options saved.';
+    console.log('saved',val);
+    // // message('Settings saved');
+    // setTimeout(function() {
+    //   status.textContent = '';
+    // }, 750);
+    
+    }
+  });
+}
+else{
+  	console.log('Error: Invalid username');
+  	status.textContent = 'Invalid username entered';
+  	// return;
+}
+}
+
+// document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
